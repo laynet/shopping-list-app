0b4392d1f470a202fd323430d3639a069afbf577
laynet <layneingramtaylor@gmail.com>
2019-05-02 11:01:04 -0500

    stopped right before props

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+import App from "./App";
+
+function App() {
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
+
+  const styles = {
+    color: "#FF8C00",
+    backgroundColor: "#FF2D00",
+    fontsize: "200px"
+  };
+  return <h1 style={styles}>Good {timeOfDay}!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
