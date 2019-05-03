81efa765fa9138c9929f96b6aafb388c879ffd69
laynet <layneingramtaylor@gmail.com>
2019-05-03 16:41:27 -0500

    EOD WC 5.3.19

---
+++ components/TodoItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function TodoItem() {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+    </div>
+  );
+}
+
+export default TodoItem;
--- index.js
+++ index.js
@@ -2,26 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  const date = new Date();
-  const hours = date.getHours();
-  let timeOfDay;
-
-  if (hours < 12) {
-    timeOfDay = "morning";
-  } else if (hours >= 12 && hours < 17) {
-    timeOfDay = "afternoon";
-  } else {
-    timeOfDay = "night";
-  }
-
-  const styles = {
-    color: "#FF8C00",
-    backgroundColor: "#FF2D00",
-    fontsize: "200px"
-  };
-  return <h1 style={styles}>Good {timeOfDay}!</h1>;
-}
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
